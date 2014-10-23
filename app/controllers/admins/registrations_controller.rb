c058602e7b3394bd71e658f371e738e60c5ef5ae
ankion <ankion@gmail.com>
2014-10-23 14:11:08 +0800

    Fixed after profile update redirect url is wrong.

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -3,6 +3,6 @@ class Admins::RegistrationsController < Devise::RegistrationsController
 
   protected
   def after_update_path_for(resource)
-    edit_admin_registration_path(resource)
+    edit_admin_registration_path
   end
 end
